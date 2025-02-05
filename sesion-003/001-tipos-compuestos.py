914c8084cc1ac072561bb2abf532abe3e518c0cc
Gladys <gladys.pg90@gmail.com>
2025-02-05 18:19:33 -0500

    Tarea definida (opcional su finalización)

---
+++ sesion-003/001-tipos-compuestos.py
@@ -0,0 +1,6 @@
+"""
+Crear 3 listas en las que estan los datos de dnis, nombres, edades
+El primer elemento de la lista dni, corresponde al primero de la lista nombres y la lista edades
+Leer un dni, si existe, darle la bienvenida saludando por su nombre
+E imprimir la edad de la persona
+"""
